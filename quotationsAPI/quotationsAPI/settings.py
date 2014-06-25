a63f713edcdbdf3d920ffc4344f7f6344b4dd866
Sean Helvey <smh436@nyu.edu>
2014-06-25 14:08:28 -0600

    created quotations app

--- quotationsAPI/quotationsAPI/settings.py
+++ quotationsAPI/quotationsAPI/settings.py
@@ -58,7 +58,11 @@ WSGI_APPLICATION = 'quotationsAPI.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'database.sql',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
