1f8aaf44ddaa0e44fdbad9ae0affff086de925b7
Sean Helvey <smh436@nyu.edu>
2014-07-03 19:24:41 -0600

    missed some changes in last commit

--- quotationsAPI/quotationsAPI/settings.py
+++ quotationsAPI/quotationsAPI/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'south',
+    'quotations',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -84,3 +87,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGINATE_BY': 10
+}
